5122405bde7ee0fa8f527e4527f63556169f5821
Hikari Genzo <leonxp@libero.it>
2019-07-07 00:07:47 +0200

    Visibile

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import time
 import os
 import sys
 import utils00 as utils_lib
-import tcp_receiver00 as receiver_lib
+#import tcp_receiver00 as receiver_lib
+import tcp_receiver01 as receiver_lib
 import log00 as log_lib
 
 basename = os.path.basename(__file__)
@@ -14,7 +15,8 @@ def main():
     for arg in sys.argv[1:]:
         pass
 
-    #time.sleep(0.234)    
+    #time.sleep(0.234)  
+    test00()
 
     elapsed_time = time.time() - start_time
     log_lib.info("Completed in %s" % utils_lib.elapsed_time_string(elapsed_time))
--- tcp_receiver01.py
+++ tcp_receiver01.py
@@ -2,8 +2,10 @@ import socket
 import log00 as log_lib
 import time
 import struct
+import numpy as np
+import cv2
 
-def tcp_receiver(remote_addr, remote_port, chunk_size = 4096, sleeping_time = 0.0001):    
+def tcp_receiver(remote_addr, remote_port, chunk_size = 4096, sleeping_time = 0.000001):    
     try:
         # Create a TCP/IP socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,7 +24,7 @@ def tcp_receiver(remote_addr, remote_port, chunk_size = 4096, sleeping_time = 0.
         real_width = int.from_bytes(globalHeader[6:9], byteorder='little', signed=False)
         real_height = int.from_bytes(globalHeader[10:13], byteorder='little', signed=False)
         virtual_width = int.from_bytes(globalHeader[14:17], byteorder='little', signed=False)
-        virtual_hieght = int.from_bytes(globalHeader[18:21], byteorder='little', signed=False)
+        virtual_height = int.from_bytes(globalHeader[18:21], byteorder='little', signed=False)
 
         display_orientation = globalHeader[22]
         quirk_flag = globalHeader[23]
@@ -33,30 +35,31 @@ def tcp_receiver(remote_addr, remote_port, chunk_size = 4096, sleeping_time = 0.
         log_lib.debug("Real Width: {}".format(real_width))
         log_lib.debug("Real Height: {}".format(real_height))
         log_lib.debug("Virtual Width: {}".format(virtual_width))
-        log_lib.debug("Virtual Height: {}".format(virtual_hieght))
+        log_lib.debug("Virtual Height: {}".format(virtual_height))
         log_lib.debug("Display Orientation: {}".format(display_orientation))
         log_lib.debug("Quirk Flag: {}".format(quirk_flag))        
 
+        window_name = str(pid) + " " + str(virtual_width) + "x" + str(virtual_height)
         debug_counter = 0
         amount_received = 0
         while True:
-            time.sleep(sleeping_time)
+            #time.sleep(sleeping_time)
             frame_size_bytes = sock.recv(4)
             frame_size = int.from_bytes(frame_size_bytes, byteorder='little', signed=False)
             time.sleep(sleeping_time)            
             frame_data = sock.recv(frame_size)
             amount_received += len(frame_data)
             log_lib.debug("Received a frame of size of {} B. Expected frame size was {}B. Total amount data received so far is {} B".format(len(frame_data), frame_size, amount_received))
-            
-            
-            
-            debug_counter += 1
-            if debug_counter > 30:
+                        
+            frame = cv2.imdecode(np.fromstring(frame_data, dtype = np.uint8), -1)            
+            cv2.imshow(window_name, frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
     except Exception as e:
         log_lib.fatal(str(e))
     finally:
+        cv2.destroyAllWindows()
         log_lib.info("Closing TCP socket {}:{} ...".format(remote_addr, remote_port))
         sock.close()   
 

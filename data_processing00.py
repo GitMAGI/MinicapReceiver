e795dede1c9c625fe3defcdf3b6c121819d15f73
Hikari Genzo <leonxp@libero.it>
2019-07-11 20:39:33 +0200

    On dev

--- data_processing00.py
+++ data_processing00.py
@@ -35,3 +35,29 @@ def frame_viewer(stop_event, queue, sleeping_time = 0.001):
     
     elapsed_time = time.time() - start_time
     log_lib.info("Completed in %s" % utils_lib.elapsed_time_string(elapsed_time))
+
+def frame_processor(stop_event, queue_in, queue_out, sleeping_time = 0.001):
+    log_lib.debug("Starting ...")
+    start_time = time.time()
+
+    while not stop_event.is_set():
+        try:
+            log_lib.debug("Trying to pull data from the Input Queue. Current Queue size {}".format(queue_input.qsize()))
+            if queue_in.qsize() == 0:           
+                time.sleep(sleeping_time)
+                continue
+
+            frame_data = queue_in.get(block = True)
+            log_lib.debug("Pulled a frame of size of {} B from Input Queue. Current Input Queue size {}".format(len(frame_data), queue.qsize()))         
+            frame = cv2.imdecode(np.fromstring(frame_data, dtype = np.uint8), -1) 
+            queue_out.put(frame_data)
+            log_lib.debug("Added data to Output Queue. Current Output Queue size {}".format(queue_out.qsize()))
+
+            time.sleep(sleeping_time)
+        
+        except Exception as e:
+            log_lib.error(str(e))
+            break
+    
+    elapsed_time = time.time() - start_time
+    log_lib.info("Completed in %s" % utils_lib.elapsed_time_string(elapsed_time))

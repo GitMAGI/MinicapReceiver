95ecd11737de806bffcb66e5a726d478b82df4a0
Hikari Genzo <leonxp@libero.it>
2019-09-02 00:08:24 +0200

    esempio di polyline

--- test07.py
+++ test07.py
@@ -23,13 +23,16 @@ def easy_drawings():
     #cv2.rectangle(img_base, (60, 100), (90, 150), (128, 12, 150), 2, 4, 0)
 
     # Polyline    
+    '''
+    t_start = 0
+    t_end = 1
     Fs = 44100
     fc = 440
-    t = np.arange(0, 1, 1/Fs)
+    t = np.arange(t_start, t_end, 1/Fs)
     A = 1
     s = A*np.cos(2*np.pi*fc*t)
     s = (s + A) / A # Shift signla in the positive range and normalize it
-    '''
+    
     i_plot_start = 4500
     i_plot_end = 4950
     t_plot = t[i_plot_start : i_plot_end]
@@ -38,29 +41,38 @@ def easy_drawings():
     plt.xlabel('time [sec]')
     plt.ylabel('Cosine fc={} sampled at Fs={}'.format(fc, Fs))
     plt.axis('tight')
-    plt.show()
-    
+    plt.show()    
     sys.stdout.write('Time ({} samples) {}\n'.format(t.size, t))
     sys.stdout.write('Cosine fc={} sampled at Fs={} ({} samples) {}\n'.format(fc, Fs, s.size, s))    
     '''
-    i_start = 4500
-    i_end = 4950
-    s_for_poly = np.round(s[i_start : i_end] * h/2)
-    t_for_poly =  np.linspace(0, i_end - i_start, num=i_end - i_start)    
 
+    t_start = 0
+    t_end = w
+    Fs = 1
+    fc = 0.04
+    t = np.arange(t_start, t_end, 1/Fs)
+    A = int(h/2)
+    s = A*np.cos(2*np.pi*fc*t)
+    s = (s + A) # Shift signla in the positive range
+    s = np.rint(s)
     '''
-    plt.plot(t_for_poly, s_for_poly)
+    i_plot_start = 0
+    i_plot_end = 512
+    t_plot = t[i_plot_start : i_plot_end]
+    s_plot = s[i_plot_start : i_plot_end]
+    plt.plot(t_plot, s_plot)
     plt.xlabel('time [sec]')
     plt.ylabel('Cosine fc={} sampled at Fs={}'.format(fc, Fs))
     plt.axis('tight')
-    plt.show()
-
-    sys.stdout.write('Resampled Time ({} samples) {}\n'.format(s_for_poly.size, s_for_poly))
-    sys.stdout.write('Resampled Cosine fc={} sampled at Fs={} ({} samples) {}\n'.format(fc, Fs, s_for_poly.size, s_for_poly))    
+    plt.show()    
+    sys.stdout.write('Time ({} samples) {}\n'.format(t.size, t))
+    sys.stdout.write('Cosine fc={} sampled at Fs={} ({} samples) {}\n'.format(fc, Fs, s.size, s))    
     '''
+    pts = np.empty([s.size, 2], dtype=np.int32)
+    for i in range(0, s.size, 1):
+        pts[i, :] = [t[i], s[i]]    
+    cv2.polylines(img_base, [pts], False, (208, 128, 80), 2, 4, 0)
 
-    #cv2.polylines(img_base, [pts], False, (208, 128, 80), 2, 4, 0)
-
-    #cv2.imshow(window_name, img_base)
-    #cv2.waitKey(0)
-    #cv2.destroyWindow(window_name)
+    cv2.imshow(window_name, img_base)
+    cv2.waitKey(0)
+    cv2.destroyWindow(window_name)
